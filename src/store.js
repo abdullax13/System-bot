25275b600bc12cae1ccb94c0c47247e5f0ac1cc2
abdullax13 <48449922+abdullax13@users.noreply.github.com>
2026-02-22 21:07:54 +0300

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,1 +1,18 @@
+// src/store.js
+const fs = require("fs");
 
+function createStore(path = "data.json") {
+  let db = {};
+  if (fs.existsSync(path)) db = JSON.parse(fs.readFileSync(path, "utf8"));
+
+  const save = () => fs.writeFileSync(path, JSON.stringify(db, null, 2));
+
+  return {
+    get: (k) => db[k],
+    set: (k, v) => { db[k] = v; save(); },
+    del: (k) => { delete db[k]; save(); },
+    all: () => Object.entries(db).map(([key, value]) => ({ key, value })),
+  };
+}
+
+module.exports = { createStore };
