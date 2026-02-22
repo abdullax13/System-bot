397641706514a4dd1fadaf813804db9e14a892bb
abdullax13 <48449922+abdullax13@users.noreply.github.com>
2026-02-22 21:21:21 +0300

    Update rulesEmbed.js

--- src/features/rulesEmbed.js
+++ src/features/rulesEmbed.js
@@ -6,7 +6,7 @@ async function ensureRulesEmbed(client, store) {
   const channel = await guild.channels.fetch(process.env.RULES_CHANNEL_ID);
 
   const serverIcon = guild.iconURL({ size: 256 });
-  const bannerUrl = process.env.https://cdn.discordapp.com/attachments/1475184112556966040/1475193445088890910/C77AAEEA-8F3B-4B83-A6A4-A893F7019FE9.png?ex=699c9875&is=699b46f5&hm=523c0fffec89ff45cc6399da4b1749253cdee5616af466d93ed2b9dea8d75516&; // رابط البنر الكبير
+  const bannerUrl = process.env.RULES_BANNER_URL; // رابط البنر الكبير
 
   const embed = new EmbedBuilder()
     .setAuthor({ name: guild.name, iconURL: serverIcon })
